5ba9f4df5e4fc925e3294e3d507d644a62d7a6ee
Marko Rodic <mrmarkorodic@googlemail.com>
2017-06-15 17:13:04 +0100

    changed headline

--- src/App.js
+++ src/App.js
@@ -4,15 +4,10 @@ import './App.css';
 
 class App extends Component {
   render() {
+    const headline = "Let's get started"
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <h2>{headline}</h2>
       </div>
     );
   }
