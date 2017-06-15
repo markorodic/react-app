cc2afd8fa76177b05d450c4cddf26df1e85feb61
Marko Rodic <mrmarkorodic@googlemail.com>
2017-06-15 17:37:40 +0100

    cleaned up list

--- src/App.js
+++ src/App.js
@@ -22,12 +22,18 @@ const list = [
 
 class App extends Component {
   render() {
-    const headline = "Let's get started"
     return (
       <div className="App">
-        { list.map(function(item) {
-          return <div>{item.title}</div>
-        })}
+        { list.map(item => 
+            <div key={item.objectID}>
+              <span>
+                <a href={item.url}>{item.title}</a>
+              </span>
+              <span>{item.author}</span>
+              <span>{item.num_comments}</span>
+              <span>{item.points}</span>
+            </div>
+        )}
       </div>
     )
   }
