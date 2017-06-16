4ae6a56c9404cf38253f497f6a04bac343f45545
Marko Rodic <mrmarkorodic@googlemail.com>
2017-06-16 15:40:18 +0100

    added button component

--- src/App.js
+++ src/App.js
@@ -97,16 +97,33 @@ class Table extends Component {
               <span>{item.num_comments}</span>
               <span>{item.points}</span>
               <span>
-                <button
-                  onClick={() => this.onDissmiss(item.objectID)}
-                  type="button"
-                >
+                <Button onClick={() => this.onDissmiss(item.objectID)}>
                   Dismiss
-                </button>
+                </Button>
               </span>
             </div>
         )}
       </div>
+    )
+  }
+}
+
+class Button extends Component {
+  render() {
+    const {
+      onClick,
+      className = '',
+      children,
+    } = this.props
+
+    return (
+      <button
+       onClick={onClick}
+       className={className}
+       type="button"
+      >
+        {children}
+      </button>
     )
   }
 }
